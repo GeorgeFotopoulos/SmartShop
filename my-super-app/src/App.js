f10bb1bb0582dc994627eccd1fd0d99416c359f6
Giorgos Fotopoulos <gfot@interleasedsa.onmicrosoft.com>
2023-02-23 00:23:04 +0200

    ignore

--- my-super-app/src/App.js
+++ my-super-app/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const fetchDatabaseData = () => {
+  const sqlite3 = require('sqlite3').verbose();
+  const db = new sqlite3.Database('../database.db');
+
+  db.serialize(() => {
+    const query = `SELECT * FROM products`;
+
+    db.all(query, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      db.close();
+      return rows;
+    });
+  });
+};
+
+const App = () => {
+  const [databaseData, setDatabaseData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchDatabaseData();
+      setDatabaseData(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Link</th>
+            <th>Product Name</th>
+            <th>Flat Price</th>
+            <th>Price per Unit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {databaseData.map((row) => (
+            <tr key={row.id}>
+              <td>{row.id}</td>
+              <td>{row.link}</td>
+              <td>{row.product_name}</td>
+              <td>{row.flat_price}</td>
+              <td>{row.price_per_unit}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
