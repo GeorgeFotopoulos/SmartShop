2b5d75f973f2e448ca453d1c71b9494d172db6d0
Giorgos Fotopoulos <gfot@interleasedsa.onmicrosoft.com>
2023-03-05 00:28:05 +0200

    ignore

--- SmartShop/SmartShop-App/main.py
+++ SmartShop/SmartShop-App/main.py
@@ -1,91 +1,23 @@
-import queue
-import random
-import threading
-import time
-
 import database_helpers
 import pandas as pd
 import scrape_helpers
 
-threads = []
-exceptions = []
-database = "database.db"
-products = queue.Queue()
-start_time = time.time()
+database_helpers.drop_database("database.db")
 data = pd.DataFrame(columns=["code", "store", "link", "product_name",
                     "starting_price", "final_price", "price_per_unit", "metric_unit", "discounted"])
 
-categories = scrape_helpers.scrape_categories(
-    "https://www.sklavenitis.gr/", "https://www.sklavenitis.gr/katigories/")
-total_progress = len(categories)
-completed_progress = 0
+products_sklavenitis = scrape_helpers.scrape_sklavenitis()
+products_ab = scrape_helpers.scrape_ab()
 
-for category in categories:
-    thread = threading.Thread(target=scrape_helpers.scrape_products, args=(
-        "https://www.sklavenitis.gr/", category, products))
-    threads.append(thread)
-    thread.start()
-
-while threads:
-    thread_count = len(threads)
-    for i in range(thread_count):
-        if not threads[i].is_alive():
-            completed_progress += 1
-            threads[i].join()
-            threads.pop(i)
-            break
-    if thread_count == len(threads):
-        time.sleep(1)
-    progress = completed_progress / total_progress
-    progress_bar_length = 50
-    filled_length = int(progress * progress_bar_length)
-    bar = "#" * filled_length + "-" * (progress_bar_length - filled_length)
-    print(f"\rScraping Σκλαβενίτης... [{bar}] {int(progress * 100)}%", end="")
-
-duration_seconds = time.time() - start_time
-minutes, seconds = divmod(duration_seconds, 60)
-print(
-    f"Scraping Σκλαβενίτης complete, runtime: {int(minutes)} minutes, {int(seconds)} seconds")
-
-start_time = time.time()
-categories_df = scrape_helpers.scrape_categories_ab(
-    "https://api.ab.gr/?operationName=LeftHandNavigationBar&variables={%22lang%22:%22gr%22}&extensions={%22persistedQuery%22:{%22version%22:1,%22sha256Hash%22:%2229a05b50daa7ab7686d28bf2340457e2a31e1a9e4d79db611fcee435536ee01c%22}}",
-)
-
-total_pages = categories_df["pages"].sum()
-completed_pages = 0
-for index, row in categories_df.iterrows():
-    category = row["category"]
-    pages = row["pages"]
-    for page in range(0, pages - 1):
-        scrape_helpers.scrape_products_ab(
-            "https://www.ab.gr",
-            f"https://api.ab.gr/?operationName=GetCategoryProductSearch&variables={{%22category%22:%22{category}%22,%22pageNumber%22:{page}}}&extensions={{%22persistedQuery%22:{{%22version%22:1,%22sha256Hash%22:%227666accda68452ba9b05424ad98dec3cd402eacd9b896718a0a932975a0f405a%22}}}}",
-            products,
-            exceptions,
-        )
-        completed_pages += 1
-        progress = completed_pages / total_pages
-        progress_bar_length = 50
-        filled_length = int(progress * progress_bar_length)
-        bar = "#" * filled_length + "-" * (progress_bar_length - filled_length)
-        print(
-            f"\rScraping ΑΒ Βασιλόπουλος... [{bar}] {int(progress * 100)}%", end="")
-        time.sleep(random.randint(2, 3))
-
-scrape_helpers.scrape_product_exceptions_ab_recursive(exceptions, products)
-while not products.empty():
+while not products_sklavenitis.empty():
     data = pd.concat(
-        [data, pd.DataFrame(products.get(), index=[0])], ignore_index=True)
+        [data, pd.DataFrame(products_sklavenitis.get(), index=[0])], ignore_index=True)
+
+while not products_ab.empty():
+    data = pd.concat(
+        [data, pd.DataFrame(products_ab.get(), index=[0])], ignore_index=True)
 
 data = data.drop_duplicates()
-
-database_helpers.drop_database(database)
-connection = database_helpers.create_database_connection(database)
+connection = database_helpers.create_database_connection("database.db")
 database_helpers.insert_data(connection, data)
 database_helpers.close_connection(connection)
-
-duration_seconds = time.time() - start_time
-minutes, seconds = divmod(duration_seconds, 60)
-print(
-    f"Scraping ΑΒ Βασιλόπουλος complete, runtime: {int(minutes)} minutes, {int(seconds)} seconds")
--- SmartShop/SmartShop-App/scrape_helpers.py
+++ SmartShop/SmartShop-App/scrape_helpers.py
@@ -1,6 +1,10 @@
 import json
 import math
+import queue
+import random
 import re
+import threading
+import time
 import urllib
 from typing import List
 from urllib.request import urlopen
@@ -9,8 +13,50 @@ import pandas
 import requests
 from bs4 import BeautifulSoup
 
+threads = []
+exceptions = []
+products_sklavenitis = queue.Queue()
+products_ab = queue.Queue()
 
-def scrape_categories(landing_page, categories_page) -> List[str]:
+
+def scrape_sklavenitis():
+    start_time = time.time()
+    categories = scrape_categories_sklavenitis(
+        "https://www.sklavenitis.gr/", "https://www.sklavenitis.gr/katigories/")
+    total_progress = len(categories)
+    completed_progress = 0
+
+    for category in categories:
+        thread = threading.Thread(target=scrape_products_sklavenitis, args=(
+            "https://www.sklavenitis.gr/", category))
+        threads.append(thread)
+        thread.start()
+
+    while threads:
+        thread_count = len(threads)
+        for i in range(thread_count):
+            if not threads[i].is_alive():
+                completed_progress += 1
+                threads[i].join()
+                threads.pop(i)
+                break
+        if thread_count == len(threads):
+            time.sleep(1)
+        progress = completed_progress / total_progress
+        progress_bar_length = 50
+        filled_length = int(progress * progress_bar_length)
+        bar = "#" * filled_length + "-" * (progress_bar_length - filled_length)
+        print(
+            f"\rScraping Σκλαβενίτης... [{bar}] {int(progress * 100)}%", end="")
+
+    duration_seconds = time.time() - start_time
+    minutes, seconds = divmod(duration_seconds, 60)
+    print(
+        f"Scraping Σκλαβενίτης complete, runtime: {int(minutes)} minutes, {int(seconds)} seconds")
+    return products_sklavenitis
+
+
+def scrape_categories_sklavenitis(landing_page, categories_page) -> List[str]:
     """
     Parses the categories within each store, in order to get the links to all categories.
 
@@ -39,7 +85,7 @@ def scrape_categories(landing_page, categories_page) -> List[str]:
     return categories
 
 
-def scrape_products(prefix, category, products):
+def scrape_products_sklavenitis(prefix, category):
     """
     Iterates all pages within a category, necessary due to pagination.
     Breaks when there are no more product links provided.
@@ -47,7 +93,6 @@ def scrape_products(prefix, category, products):
     Parameters:
         prefix (Literal): The category url that contains all objects that will be scraped for data.
         category (str): A particular category that will be parsed for all its products' data to be scraped.
-        products (Queue): A queue to temporarily hold the data, because of thread locking.
     """
 
     i = 1
@@ -64,18 +109,17 @@ def scrape_products(prefix, category, products):
             continue
 
         for product in products_list:
-            scrape_data(prefix, products, product)
+            scrape_data_sklavenitis(prefix, product)
 
         i += 1
 
 
-def scrape_data(prefix, products, product):
+def scrape_data_sklavenitis(prefix, product):
     """
     Scrapes product link, name, flat price and price per unit.
 
     Parameters:
         prefix (Literal): The prefix to add to the url for each particular product.
-        products (Queue): A queue to temporarily hold the data, because of thread locking.
         product (BeautifulSoup): A particular product's soup variable, to extract the data from.
     """
 
@@ -137,7 +181,42 @@ def scrape_data(prefix, products, product):
         "metric_unit": metric_unit,
         "discounted": 0 if starting_price == final_price else 1,
     }
-    products.put(new_row)
+    products_sklavenitis.put(new_row)
+
+
+def scrape_ab():
+    start_time = time.time()
+    categories_df = scrape_categories_ab(
+        "https://api.ab.gr/?operationName=LeftHandNavigationBar&variables={%22lang%22:%22gr%22}&extensions={%22persistedQuery%22:{%22version%22:1,%22sha256Hash%22:%2229a05b50daa7ab7686d28bf2340457e2a31e1a9e4d79db611fcee435536ee01c%22}}",
+    )
+
+    total_pages = categories_df["pages"].sum()
+    completed_pages = 0
+    for index, row in categories_df.iterrows():
+        category = row["category"]
+        pages = row["pages"]
+        for page in range(0, pages - 1):
+            scrape_products_ab(
+                "https://www.ab.gr",
+                f"https://api.ab.gr/?operationName=GetCategoryProductSearch&variables={{%22category%22:%22{category}%22,%22pageNumber%22:{page}}}&extensions={{%22persistedQuery%22:{{%22version%22:1,%22sha256Hash%22:%227666accda68452ba9b05424ad98dec3cd402eacd9b896718a0a932975a0f405a%22}}}}",
+                exceptions,
+            )
+            completed_pages += 1
+            progress = completed_pages / total_pages
+            progress_bar_length = 50
+            filled_length = int(progress * progress_bar_length)
+            bar = "#" * filled_length + "-" * \
+                (progress_bar_length - filled_length)
+            print(
+                f"\rScraping ΑΒ Βασιλόπουλος... [{bar}] {int(progress * 100)}%", end="")
+            time.sleep(random.uniform(1, 2))
+
+    scrape_product_exceptions_ab_recursive(exceptions)
+    duration_seconds = time.time() - start_time
+    minutes, seconds = divmod(duration_seconds, 60)
+    print(
+        f"Scraping ΑΒ Βασιλόπουλος complete, runtime: {int(minutes)} minutes, {int(seconds)} seconds")
+    return products_ab
 
 
 def scrape_categories_ab(url):
@@ -165,7 +244,7 @@ def scrape_categories_ab(url):
     return categories
 
 
-def scrape_products_ab(landing_page, url, products, exceptions):
+def scrape_products_ab(landing_page, url, exceptions):
     """
     Iterates all pages within a category, necessary due to pagination.
     Breaks when there are no more product links provided.
@@ -234,20 +313,20 @@ def scrape_products_ab(landing_page, url, products, exceptions):
                 "metric_unit": metric_unit,
                 "discounted": 0 if not final_price or (final_price and starting_price == float(final_price)) else 1,
             }
-            products.put(new_row)
+            products_ab.put(new_row)
     except urllib.error.URLError as e:
         exceptions.append(url)
 
 
-def scrape_product_exceptions_ab_recursive(exceptions, products):
+def scrape_product_exceptions_ab_recursive(exceptions):
     """ TODO """
     exceptions_new = []
     for url in exceptions:
         try:
             scrape_products_ab("https://www.ab.gr", url,
-                               products, exceptions_new)
+                               products_ab, exceptions_new)
         except (urllib.error.URLError, KeyError):
             exceptions.append(url)
 
     if exceptions_new:
-        scrape_product_exceptions_ab_recursive(exceptions_new, products)
+        scrape_product_exceptions_ab_recursive(exceptions_new, products_ab)
