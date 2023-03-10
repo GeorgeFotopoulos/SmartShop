efdadd4f1450ff1971a9967e96d26b8c4ce77818
George Fotopoulos <giorgos.fotopoulos7@gmail.com>
2023-03-10 09:29:50 +0200

    ignore

--- SmartShop/SmartShop-App/correlation_helpers.py
+++ SmartShop/SmartShop-App/correlation_helpers.py
@@ -1,6 +1,5 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.neighbors import KNeighborsClassifier
 
 
